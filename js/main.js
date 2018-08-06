fb7bdf651b315c3dbc7857c4301d7a40b3d3b513
AlexSandr90 <gayrsuk@gmail.com>
2018-08-06 19:33:32 +0300

    optimize code

--- js/main.js
+++ js/main.js
@@ -23,21 +23,20 @@
 */
 
 function startChance(min, max) {
-    let random = Math.round(min - 0.5 + Math.random() * (max - min + 1));
-    return random;
-};
+    return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+}
 
 function chanceHitting(steps) {
     let stepLeft = steps;
     let stepCount = 0;
     let initialChance = startChance(10, 30);
-    console.log(`start chance: ${initialChance} %`)
+    console.log(`start chance: ${initialChance} %`);
     for (let i = 0; i < steps; i++) {
         initialChance = ((100 - initialChance) / stepLeft) + initialChance;
         stepLeft--;
         stepCount++;
         console.log(`Step: ${stepCount}; chance of hitting: ${initialChance} %`);
-    };
-};
+    }
+}
 
 chanceHitting(30);
